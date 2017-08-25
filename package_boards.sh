11f1b680026bee06067343a7e5106c200ce6577c
ddrd <ddrd001@gmail.com>
2017-08-25 18:40:28 +0300

    Added packager script.

---
+++ package_boards.sh
@@ -0,0 +1,4 @@
+VERSION=1.1.1
+rm dist/PLDuino???-$VERSION.*
+tar -zcvf dist/PLDuinoAVR-$VERSION.tar.gz plduino-arduinoide-package/PLDuinoAVR
+tar -zcvf dist/PLDuinoESP-$VERSION.tar.gz plduino-arduinoide-package/PLDuinoESP
